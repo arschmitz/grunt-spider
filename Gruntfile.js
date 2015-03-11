2345921f340fc9facc10130681a4c0174b7d26cd
Alexander Schmitz <arschmitz@gmail.com>
2015-03-11 09:05:16 -0400

    Update to use spider.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,23 +6,21 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
 module.exports = function(grunt) {
+
+	// grunt plugins
+	require( "load-grunt-tasks" )( grunt );
+
 	// Project configuration.
 	grunt.initConfig({
 		jshint: {
 			all: [
-				'Gruntfile.js',
-				'tasks/*.js'
+				"Gruntfile.js",
+				"tasks/*.js"
 			],
 			options: {
-				jshintrc: '.jshintrc'
+				jshintrc: ".jshintrc"
 			}
-		},
-
-		// Before generating any new files, remove any previously-created files.
-		clean: {
-			tests: ['tmp']
 		},
 
 		// Configuration to be run (and then tested).
@@ -30,13 +28,16 @@ module.exports = function(grunt) {
 			"dev" : {
 				options: {
 					url:  "http://jquery-ui.10.0.1.29.xip.io/demos/",
-					ignore: ""
+					output: "./log.txt",
+					ignore: "",
+					verbose: true
 				}
 			},
 			"stage" : {
 				options: {
 					url:  "http://stage.jqueryui.com/",
-					ignore: "broken|trash"
+					ignore: "broken|trash",
+					verbose: true
 				}
 			},
 			"production" : {
@@ -48,15 +49,10 @@ module.exports = function(grunt) {
 
 	});
 
-	// Actually load this plugin's task(s).
-	grunt.loadTasks('tasks');
-	// grunt plugins
-	require( "load-grunt-tasks" )( grunt );
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-casper');
+	// Actually load this plugin"s task(s).
+	grunt.loadTasks("tasks");
 
 	// By default, lint and run all tests.
-	grunt.registerTask('default', ['jshint', 'spider']);
+	grunt.registerTask("default", [ "jshint", "spider" ]);
 
 };
--- tasks/spider.js
+++ tasks/spider.js
@@ -8,36 +8,15 @@
 
 'use strict';
 
-var path = require( "path" );
-
 module.exports = function(grunt) {
 
-	// Please see the Grunt documentation for more information regarding task
-	// creation: http://gruntjs.com/creating-tasks
+	grunt.registerMultiTask( "spider", 'A tool to spider a website and check for any broken links resources load errors or script errors', function() {
 
-	grunt.registerMultiTask('spider', 'A tool to spider a website and check for any broken links resources load errors or script errors', function() {
-	// Merge task-specific and/or target-specific options with these defaults.
-		var casperOptions = {},
-			options = this.options({
-			url: "http://localhost",
-			ignore: "",
-			script: true,
-			resource: true,
-			links: true,
-			casper : {
-				test: true,
-				verbose : true,
-				"log-level": "error",
-				parallel: false
-			}});
+		var spider = require( "spider.js" ),
+			done = this.async();
 
-		options.casper.args = [ "--url=" + options.url , "--ignore=" + options.ignore ];
-
-		grunt.config( "casper." + this.target, {
-			"options": options.casper,
-			src: [ "tasks/lib/spider.js" ]
-		});
-		grunt.task.run([ "casper:" + this.target ]);
+		console.log( this.options() )
+		spider( this.options(), done );
 	});
 
 };
